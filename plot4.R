04509fa18cedf6a845c04e4d4436575d643dc0ab
ngg971 <ngg971@gmail.com>
2018-09-30 21:13:18 -0400

    Misc

---
+++ plot4.R
@@ -0,0 +1,32 @@
+# Read in data, accounting for NA symbols
+power_data <- read.delim('household_power_consumption.txt', sep = ";", na.strings = "?", header = TRUE)
+
+# Format Dates
+power_data$Date <- as.Date(power_data$Date, format="%d/%m/%Y")
+
+# Select subset of data for the days we want
+power_data_selected_days <- subset(power_data, Date == as.Date("2007-02-01 00:00:00") | Date == as.Date("2007-02-02 00:00:00"))
+
+# Generate datetime column
+power_data_selected_days$DateTime <- strptime(paste(power_data_selected_days$Date, power_data_selected_days$Time), format='%Y-%m-%d %H:%M:%S')
+
+# Set plot area
+par(mfrow=c(2,2))
+
+# Add first plot
+plot(power_data_selected_days$DateTime, power_data_selected_days$Global_active_power, type="o", pch=NA, xlab="", ylab="Global Active Power (kilowatts)")
+    
+# Add second plot
+plot(power_data_selected_days$DateTime, power_data_selected_days$Voltage, type="o", pch=NA, xlab="", ylab="Voltage")
+
+# Add third plot
+plot(power_data_selected_days$DateTime, power_data_selected_days$Sub_metering_1, type="o", pch=NA, xlab="", ylab="Energy sub metering")
+lines(power_data_selected_days$DateTime, power_data_selected_days$Sub_metering_2, col="red")
+lines(power_data_selected_days$DateTime, power_data_selected_days$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("Black", "Red", "Blue"), lty=1)
+
+# Add fourth plot
+plot(power_data_selected_days$DateTime, power_data_selected_days$Global_reactive_power, type="o", pch=NA, xlab="", ylab="Global Reactive Power (kilowatts)")
+
+# Save plot
+dev.copy(png, file="plot4.png")
